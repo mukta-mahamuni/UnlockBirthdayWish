b432aa079bb0d27bed80a6f8dd2e38da4d36ae7c
Mukta Mahamuni <160267116+mukta-mahamuni@users.noreply.github.com>
2025-12-04 00:57:45 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,10 +1,13 @@
+
 export enum GameState {
   INTRO = 'INTRO',
   MISSION_1 = 'MISSION_1', // Riddle
   MISSION_2 = 'MISSION_2', // Emojis
   MISSION_3 = 'MISSION_3', // Quiz
   UNLOCK = 'UNLOCK',       // Final Password
-  REVEAL = 'REVEAL',       // Birthday Wish
+  CAKE = 'CAKE',           // New: Blow candle & cut cake
+  GIFT = 'GIFT',           // New: Open gift box
+  REVEAL = 'REVEAL',       // Birthday Wish & Gallery
 }
 
 export interface RiddleResponse {
