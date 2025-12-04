b281a45b9292e04aa9766f5e83b6be2c5d95571b
Mukta Mahamuni <160267116+mukta-mahamuni@users.noreply.github.com>
2025-12-04 15:32:03 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,11 +2,13 @@
 export enum GameState {
   INTRO = 'INTRO',
   MISSION_1 = 'MISSION_1', // Riddle
-  MISSION_2 = 'MISSION_2', // Emojis
-  MISSION_3 = 'MISSION_3', // Quiz
+  MISSION_2 = 'MISSION_2', // Memory
+  MISSION_3 = 'MISSION_3', // Water Meter (New)
+  MISSION_4 = 'MISSION_4', // Emojis (Restored)
+  MISSION_5 = 'MISSION_5', // Quiz (Previously Mission 3)
   UNLOCK = 'UNLOCK',       // Final Password
-  CURTAIN = 'CURTAIN',     // New: Theater curtain
-  PARTY = 'PARTY',         // New: Cartoons & Fireworks
+  CURTAIN = 'CURTAIN',     // Theater curtain
+  PARTY = 'PARTY',         // Cartoons & Fireworks
   CAKE = 'CAKE',           // Blow candle & cut cake
   GIFT = 'GIFT',           // Open gift box
   REVEAL = 'REVEAL',       // Birthday Wish & Gallery
